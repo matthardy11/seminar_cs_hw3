7c0a8d2f06ac9aa85d7576ca1989b9754f34ca06
matthardy11 <matthardy2560@gmail.com>
2023-01-26 17:51:22 +0400

    complete task1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+System.Console.WriteLine("Введите пятизначное число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[5];
+
+if (number < 10000 || number > 99999)
+{
+  System.Console.WriteLine("Число не пятизначное");
+  
+}
+else
+{
+  int counter = 4;
+  while(number > 0)
+  {
+    array[counter] = number%10;
+    number = number/10;
+    counter--;
+  }
+  
+  if (array[0] == array[4] && array[1] == array[3])
+  {
+    System.Console.WriteLine("Число является палиндромом");
+  } 
+  else
+  {
+    System.Console.WriteLine("Число не является палиндромом");
+  }
+}
